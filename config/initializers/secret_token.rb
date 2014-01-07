4fa68250cd9eb0cc02f23e2eb396e987f357a62d
Masahiro Nishimi <nishimi@sonicgarden.jp>
2014-01-07 09:21:27 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWerckerHeroku::Application.config.secret_key_base = '7dc899efcf975feaad97bca63427201dcfcd0035c0f0a3be7124b7ce6fb3ec5236dcfa7706a11e5ec701e957f44569b3103f974a748915a381180b4087431a53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsWerckerHeroku::Application.config.session_store :cookie_store, key: '_rails-wercker-heroku_session'
